4a78082be08343d80e2ece837e734a33b211710f
Rax Canaan Layumas <raxkaynan@gmail.com>
2020-11-04 15:04:41 +0800

    ENG-1437 Improve content edit button

--- src/main/resources/sql/plugins/jacms/port_data_production.sql
+++ src/main/resources/sql/plugins/jacms/port_data_production.sql
@@ -254,12 +254,34 @@ INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, lock
 <@jacms.contentInfo param="authToEdit" var="canEditThis" />
 <@jacms.contentInfo param="contentId" var="myContentId" />
 <#if (canEditThis?? && canEditThis)>
-	<div class="bar-content-edit">
-		<a href="<@wp.info key="systemParam" paramName="applicationBaseURL" />do/jacms/Content/edit.action?contentId=<@jacms.contentInfo param="contentId" />" class="btn btn-info">
-		<@wp.i18n key="EDIT_THIS_CONTENT" /> <i class="icon-edit icon-white"></i></a>
+	<style>
+		.bar-content-edit {
+				position: absolute;
+				transform: translate(4px, 20px);
+		}
+		.bar-content-edit .btn {
+				color: white;
+				padding: 12px 32px;
+				background: #33B5E5;
+				text-transform: uppercase;
+				text-decoration: none;
+				border-radius: 2px;
+				box-shadow: 4px 4px 8px 1px rgba(0,0,0,0.5);
+		}
+		.bar-content-edit-container:not(:hover) .bar-content-edit {
+				display: none;
+		}
+	</style>
+	<div class="bar-content-edit-container">
+		<div class="bar-content-edit">
+			<a href="<@wp.info key="systemParam" paramName="applicationBaseURL" />do/jacms/Content/edit.action?contentId=<@jacms.contentInfo param="contentId" />" class="btn btn-info">
+			<@wp.i18n key="EDIT_THIS_CONTENT" /> <i class="icon-edit icon-white"></i></a>
+		</div>
+		<@jacms.content publishExtraTitle=true />
 	</div>
-</#if>
-<@jacms.content publishExtraTitle=true />', 1);
+<#else>
+  <@jacms.content publishExtraTitle=true />
+</#if>', 1);
 INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked) VALUES ('jacms_content_viewer_list', 'content_viewer_list', 'jacms', NULL, '<#assign jacms=JspTaglibs["/jacms-aps-core"]>
 <#assign wp=JspTaglibs["/aps-core"]>
 <@wp.headInfo type="JS_EXT" info="http://code.jquery.com/ui/1.10.3/jquery-ui.min.js" />
