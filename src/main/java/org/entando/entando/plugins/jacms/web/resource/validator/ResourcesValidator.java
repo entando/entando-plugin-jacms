1e721cd68628c12ff8a39a9fe24d1fec6bb79f01
Alessio Pintus <alessio.pintus@gmail.com>
2020-12-18 15:58:13 +0100

    ENG-1578 add validation for referenced resources in the delete assets API

--- src/main/java/org/entando/entando/plugins/jacms/web/resource/validator/ResourcesValidator.java
+++ src/main/java/org/entando/entando/plugins/jacms/web/resource/validator/ResourcesValidator.java
@@ -13,25 +13,38 @@
  */
 package org.entando.entando.plugins.jacms.web.resource.validator;
 
-import org.entando.entando.ent.exception.EntException;
 import com.agiletec.aps.system.services.user.UserDetails;
+import com.agiletec.plugins.jacms.aps.system.services.content.ContentManager;
 import com.agiletec.plugins.jacms.aps.system.services.content.helper.BaseContentListHelper;
+import com.agiletec.plugins.jacms.aps.system.services.content.model.ContentRecordVO;
 import com.agiletec.plugins.jacms.aps.system.services.resource.ResourceManager;
+import com.agiletec.plugins.jacms.aps.system.services.resource.ResourceUtilizer;
 import com.agiletec.plugins.jacms.aps.system.services.resource.model.ResourceInterface;
-import java.util.Collection;
 import org.entando.entando.aps.util.GenericResourceUtils;
+import org.entando.entando.ent.exception.EntException;
 import org.entando.entando.plugins.jacms.web.resource.model.ImageAssetDto;
 import org.entando.entando.web.common.validator.AbstractPaginationValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 
+import java.util.Collection;
+import java.util.List;
+
 @Component
 public class ResourcesValidator extends AbstractPaginationValidator {
 
     @Autowired
     private ResourceManager resourceManager;
-    
+
+    @Autowired
+    private ContentManager contentManager;
+
+    private final static String ERR_CODE_RESOURCE_REFERENCED_CONTENT_ERROR ="20";
+    private final static String ERR_CODE_RESOURCE_REFERENCED_NOT_DELETABLE ="21";
+    private final static String ERR_MSG_RESOURCE_REFERENCED_CONTENT_ERROR ="plugins.jacms.resources.delete.error.contentError";
+    private final static String ERR_MSG_RESOURCE_REFERENCED_NOT_DELETABLE ="plugins.jacms.resources.delete.error.referenced";
+
     public boolean isResourceDeletableByUser(String resourceId, String correlationCode, UserDetails user) throws EntException {
         final Collection<String> allowedGroupCodes = BaseContentListHelper.getAllowedGroupCodes(user);
         final ResourceInterface resource = resourceManager.loadResource(resourceId, correlationCode);
@@ -50,6 +63,23 @@ public class ResourcesValidator extends AbstractPaginationValidator {
             return true;
         }
         return false;
+    }
+
+    public void resourceReferencesValidation(String resourceId, Errors errors) throws EntException {
+        List<String> references = ((ResourceUtilizer) contentManager).getResourceUtilizers(resourceId);
+        if (references != null && references.size() > 0) {
+            references.forEach(reference->{
+                ContentRecordVO record = null;
+                try {
+                    record = contentManager.loadContentVO(reference);
+                } catch (EntException e) {
+                    errors.reject(ERR_CODE_RESOURCE_REFERENCED_CONTENT_ERROR, new String[]{resourceId, record.getDescription()}, ERR_MSG_RESOURCE_REFERENCED_CONTENT_ERROR);
+                }
+                if (null != record) {
+                    errors.reject(ERR_CODE_RESOURCE_REFERENCED_NOT_DELETABLE, new String[]{resourceId, record.getId(), record.getDescription()}, ERR_MSG_RESOURCE_REFERENCED_NOT_DELETABLE);
+                }
+                });
+            }
     }
 
     @Override
